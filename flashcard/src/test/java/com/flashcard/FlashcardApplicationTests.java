a64b71a6fdeef7224b29342db77e8bb26e050d56
yamato <kuwayama080@gmail.com>
2023-04-21 00:09:38 +0000

    feat: first commit

---
+++ flashcard/src/test/java/com/flashcard/FlashcardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flashcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
