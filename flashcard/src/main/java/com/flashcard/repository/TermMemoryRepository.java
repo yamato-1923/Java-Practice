d2b5b3cd215ead32a79233243fd844adf1727a04
yamato <kuwayama080@gmail.com>
2023-05-01 02:14:14 +0000

    feat: メモリで単語を記録するリポジトリクラスを追加

---
+++ flashcard/src/main/java/com/flashcard/repository/TermMemoryRepository.java
@@ -0,0 +1,21 @@
+package com.flashcard.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.flashcard.entity.Term;
+
+@Repository
+public class TermMemoryRepository {
+    private List<Term> termList = new ArrayList<Term>();
+
+    public Boolean insertTerm(Term term) {
+        return termList.add(term);
+    }
+
+    public List<Term> getTermList() {
+        return this.termList;
+    }
+}
